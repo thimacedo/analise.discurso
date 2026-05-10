bef5196137212cb5d3756242a15048d8a6a00aff
Thiago Macedo <thi.macedo@gmail.com>
2026-05-10 06:03:08 -0300

    rb

--- src/config.js
+++ src/config.js
@@ -9,4 +9,3 @@ export const SENTINELA_CONFIG = {
     supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoYW1lamtsZHp4YmVpYnFlcXBrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NjQ4ODEyNSwiZXhwIjoyMDkyMDY0MTI1fQ.GfvAI7rV8isgdhVeJp4mOUscWpdOqOuBoURGm82VdtY',
     refreshInterval: 3600000
 };
-window.SENTINELA_CONFIG = SENTINELA_CONFIG;
